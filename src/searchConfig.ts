2626d80facb01d5ecee5d1475c73a96afbd56a97
Keerthi Niranjan <keerthiniranjan16@gmail.com>
2018-07-20 11:57:09 +0530

    Updated path to utils

--- src/searchConfig.ts
+++ src/searchConfig.ts
@@ -23,7 +23,7 @@ const userConfigFile = isDevEnv ? path.join(__dirname, '..', userConfigFileName)
 
 const libraryFolderPath = isMac ? macLibraryPath : winLibraryPath;
 
-const pathToUtils = isDevEnv ? path.join(__dirname, '../node_modules/electron-utils') : winLibraryPath;
+const pathToUtils = isDevEnv ? path.join(__dirname, '../../node_modules/electron-utils') : winLibraryPath;
 const freeDiskSpace = path.join(pathToUtils, isDevEnv ? 'FreeDiskSpace/bin/Release/FreeDiskSpace.exe' : 'FreeDiskSpace.exe');
 
 const libraryPaths = {
