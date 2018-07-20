35168417efd581ba0463e87d4b55d061cceca1ae
Keerthi Niranjan <keerthiniranjan16@gmail.com>
2018-07-20 14:48:30 +0530

    Added lint to build script Updates demo page

--- demo/preload.ts
+++ demo/preload.ts
@@ -1,7 +1,24 @@
 import { remote } from 'electron';
-const SwiftSearch = remote.require('../lib/index');
+
+let Search;
+
+try {
+    Search = remote.require('../lib/index').Search;
+} catch (e) {
+    Search = null;
+    console.warn('Failed to initialize');
+}
+
+let SearchUtils;
+
+try {
+    SearchUtils = remote.require('../lib/index').SearchUtils;
+} catch (e) {
+    SearchUtils = null;
+    console.warn('Failed to initialize');
+}
 
 window['ssf'] = {
-    Search: SwiftSearch.Search,
-    SearchUtils: SwiftSearch.SearchUtils,
+    Search: Search,
+    SearchUtils: SearchUtils,
 };
