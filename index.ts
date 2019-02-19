a09f9b7609a81a2ad42dfc7c1047a5d835d072a3
Keerthi Niranjan <keerthiniranjan16@gmail.com>
2019-02-19 17:25:13 +0530

    SEARCH-1313 - Initial setup

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 'use strict';
 
 import Search from './src/search';
+import SSAPIBridge from './src/searchAPIBridge';
 import SearchUtils from './src/utils/searchUtils';
 
 export {
     Search,
     SearchUtils,
+    SSAPIBridge,
 };
